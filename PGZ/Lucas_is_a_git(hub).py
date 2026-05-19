b4f57336f3a1e489207936f5f569ab2a908d1746
20allsoppj <20allsoppj@jmf6.org>
2026-05-19 10:30:05 +0100

    FINISHED

--- PGZ/Lucas_is_a_git(hub).py
+++ PGZ/Lucas_is_a_git(hub).py
@@ -1,23 +1,4 @@
-import json
+my_string = "LUCAS IS A GIT"
 
-name = input("Enter name")#
-age = input("Enter age")
-street_num = input("Enter street number")
-post_code = input("Enter postcode")
-
-
-user_data = {"name" : name,
-             "age" : age,
-             "street_num" : street_num,
-             "post_code" : post_code}
-
-with open("json_data.json", "w") as file:
-    json.dump(user_data,file,indent=4)
-
-with open("json_data.json", "r") as file:
-    new_user_dict = json.load(file)
-
-print(new_user_dict)
-
-
-print("I AM FINISHED")
+for i in range(len(my_string)):
+    print(my_string[i])
