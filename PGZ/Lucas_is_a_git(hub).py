89d05a02e8fbbec3909549288d110abf29db54de
20allsoppj <20allsoppj@jmf6.org>
2026-05-19 10:24:25 +0100

    FINISHED

--- PGZ/Lucas_is_a_git(hub).py
+++ PGZ/Lucas_is_a_git(hub).py
@@ -1,1 +1,23 @@
-print("hello world")
+import json
+
+name = input("Enter name")#
+age = input("Enter age")
+street_num = input("Enter street number")
+post_code = input("Enter postcode")
+
+
+user_data = {"name" : name,
+             "age" : age,
+             "street_num" : street_num,
+             "post_code" : post_code}
+
+with open("json_data.json", "w") as file:
+    json.dump(user_data,file,indent=4)
+
+with open("json_data.json", "r") as file:
+    new_user_dict = json.load(file)
+
+print(new_user_dict)
+
+
+print("I AM FINISHED")
